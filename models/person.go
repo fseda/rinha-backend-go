90825506351b23453f716c672f3950f018f8de2e
fseda <felipeseda@poli.ufrj.br>
2023-08-25 10:45:22 -0300

    Change stack type to pq.StringArray

--- models/person.go
+++ models/person.go
@@ -4,17 +4,18 @@ import (
 	"errors"
 
 	"github.com/google/uuid"
+	"github.com/lib/pq"
 )
 
 var (
-	ErrNicknameTaken = errors.New("nickname taken")
+	ErrNicknameTaken  = errors.New("nickname taken")
 	ErrPersonNotFound = errors.New("person not found")
 )
 
 type Person struct {
-	ID        uuid.UUID `json:"id"`
-	Nickname  string    `json:"apelido"`
-	Name      string    `json:"nome"`
-	Birthdate string    `json:"nascimento"`
-	Stack     []string  `json:"stack"`
+	ID        uuid.UUID      `json:"id"`
+	Nickname  string         `json:"apelido"`
+	Name      string         `json:"nome"`
+	Birthdate string         `json:"nascimento"`
+	Stack     pq.StringArray `json:"stack"`
 }
